b4481ca054b55485e8df23d8be213839ccf9eca3
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2024-11-13 17:28:04 +0100

    last commit

--- src/main/java/es/uniovi/avib/morphing/projections/backend/storage/UnioviAvibMorphingprojectionsBackendStorageApplication.java
+++ src/main/java/es/uniovi/avib/morphing/projections/backend/storage/UnioviAvibMorphingprojectionsBackendStorageApplication.java
@@ -2,10 +2,9 @@ package es.uniovi.avib.morphing.projections.backend.storage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UnioviAvibMorphingprojectionsBackendStorageApplication {
@@ -13,19 +12,9 @@ public class UnioviAvibMorphingprojectionsBackendStorageApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UnioviAvibMorphingprojectionsBackendStorageApplication.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsMappingConfigurer() {
-	   return new WebMvcConfigurer() {
-	       @Override
-	       public void addCorsMappings(CorsRegistry registry) {	           
-	           registry.addMapping("/**");
-	       }
-	   };
-	}
 	
-	@Bean
-	public RestTemplate restTemplate() {
-	    return new RestTemplate();
-	}	
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }	
 }
